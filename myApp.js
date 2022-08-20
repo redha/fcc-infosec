dc2ef95158e351348d4ade8b168c7beb5a02e491
Redha Achour <redhaperso@gmail.com>
2022-08-20 21:54:16 +0100

    use Helmet in one command

--- myApp.js
+++ myApp.js
@@ -1,87 +1,98 @@
 const express = require('express');
+const { frameguard } = require('helmet');
 const helmet = require('helmet');
 const app = express();
 
 // USIING Helmet ...
 
+// Hide PoweredBy header
+/*
 app.use(helmet.hidePoweredBy());
+*/
+
+// hey browser ! Please deny any framing (frame / iframe) of my website 
+/*
 app.use(helmet.frameguard({action: 'deny'}));
+*/
+
+// xssFilter
+/*
 app.use(helmet.xssFilter());
+*/
+
+// noSniff
+/*
 app.use(helmet.noSniff());
+*/
 
 // Some web applications will serve untrusted HTML for download. Some versions of Internet Explorer by default open those HTML files in the context of your site. This means that an untrusted HTML page could start doing bad things in the context of your pages. This middleware sets the X-Download-Options header to noopen. This will prevent IE users from executing downloads in the trusted site’s context.
+/*
 app.use(helmet.ieNoOpen());
+*/
 
 // Ask Browsers to Access Your Site via HTTPS Only with helmet.hsts()
 // HTTP Strict Transport Security (HSTS) is a web security policy which helps to protect websites against protocol downgrade attacks and cookie hijacking. If your website can be accessed via HTTPS you can ask user’s browsers to avoid using insecure HTTP. By setting the header Strict-Transport-Security, you tell the browsers to use HTTPS for the future requests in a specified amount of time. This will work for the requests coming after the initial request.
+/*
 app.use(helmet.hsts({
   maxAge: 90*24*60*60, // hey browser ! Please remember this parameter for the next 90 days.
   force: true  // This will insure overwrite any host parameter (heroku, repl,...) / subdomains ??
 }));
+*/
 
 // Disable DNS Prefetching with helmet.dnsPrefetchControl()
 // To improve performance, most browsers prefetch DNS records for the links in a page. In that way the destination ip is already known when the user clicks on a link. This may lead to over-use of the DNS service (if you own a big website, visited by millions people…), privacy issues (one eavesdropper could infer that you are on a certain page), or page statistics alteration (some links may appear visited even if they are not). If you have high security needs you can disable DNS prefetching, at the cost of a performance penalty.
+/*
 app.use(helmet.dnsPrefetchControl());
+*/
 
 
 // Disable Client-Side Caching with helmet.noCache()
 // If you are releasing an update for your website, and you want the users to always download the newer version, you can (try to) disable caching on client’s browser. It can be useful in development too. Caching has performance benefits, which you will lose, so only use this option when there is a real need.
+
+/*
 app.use(helmet.noCache());
+*/
 
 
 // Set a Content Security Policy with helmet.contentSecurityPolicy()
 // By default, directives are wide open, so it’s important to set the defaultSrc directive as a fallback. Helmet supports both defaultSrc and default-src naming styles. The fallback applies for most of the unspecified directives.
+/*
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
     scriptSrc: ["'self'", 'trusted-cdn.com'],
   }
 }));
+*/
+
+// By Default, Helmet Takes care of the following : 
+// - PoweredBy 
+// - xssFilter
+// - noSniff
+// - ieNoOpen
+// - hsts
+// - nocache
+// But 
+// - dnsPrefetchControl set to false
+// - frameguard but default value isn't set to deny
+// - contentSecurityPolicy isn't enabled 
+
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      // styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false
+}))
 
 /*
 https://boilerplate-infosec.redhaachour.repl.co
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = app;
